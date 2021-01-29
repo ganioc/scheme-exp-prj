41ce8e91dfdcb84a81df44a68219a3d791fe0ede
yango3 <o3.yang@hotmail.com>
2021-01-29 14:39:09 +0800

    update chap 10

--- simply/chap10.scm
+++ simply/chap10.scm
@@ -1,16 +1,17 @@
 
-(define (my-ttt position me)
-  (cond ((i-can-win?)
-	 (choose-winning-move))
-	((opponent-can-win?)
-	 (block-opponent-win))
-	((i-can-win-next-time?)
-	 (prepare-win))
-	(else
-	 (whatever)))
-  )
+;; (define (my-ttt position me)
+;;   (cond ((i-can-win?)
+;; 	 (choose-winning-move))
+;; 	((opponent-can-win?)
+;; 	 (block-opponent-win))
+;; 	((i-can-win-next-time?)
+;; 	 (prepare-win))
+;; 	(else
+;; 	 (whatever)))
+;;   )
 
 (define (my-substitute-letter square position)
+  ;; if equals _ , return position, 
   (if (equal? '_ (item position square))
       position
       (item position square))
@@ -22,4 +23,15 @@
 		       (my-substitute-letter position  square ))
 		     combination))
   )
+(define (find-triples position)
+  (every (lambda (comb) (substitute-triple comb position))
+	 '(123 456 789 147 258 369 159 357)))
+
+(define (opponent letter)
+  (if (equal? letter 'x) 'o 'x))
+
+;;
+(define (my-pair? triple me)
+(and (= (appearances me triple) 2)
+     (= (appearances (opponent me) triple) 0)))
 
