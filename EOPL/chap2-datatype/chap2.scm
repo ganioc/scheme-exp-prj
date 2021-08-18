ca148dcdfacf5a724fd48970c6d951fee0f51918
yango3 <o3.yang@hotmail.com>
2021-08-18 22:49:23 +0800

    update version 1, chap2 exercise

--- EOPL/chap2-datatype/chap2.scm
+++ EOPL/chap2-datatype/chap2.scm
@@ -6,3 +6,35 @@
 	(cons n (duple (- x 1) n)))
     ))
 
+(define invert
+  (lambda (lst)
+    (if (null? lst)
+	'()
+	(let ([a (car lst)])
+	  (cons  (cons (car (cdr a))  (car a) ) 
+		(invert (cdr lst)))))))
+(define cust-list-index
+  (lambda (s los n)
+    (if (null? los)
+	-1
+	(if (eq? s (car los))
+	    n
+	    (cust-list-index s (cdr los) (+ 1 n)))
+	) 
+    ))
+
+(define list-index
+  (lambda ( s los)
+    (cust-list-index s los 0)
+    ))
+(define cust-list-ref
+  (lambda (los index n)
+    (if (eq? index n)
+	(car los)
+	(cust-list-ref (cdr los) index (+ 1 n)))
+    ))
+
+(define list-ref
+  (lambda (los index)
+    (cust-list-ref los index 0)
+    ))
